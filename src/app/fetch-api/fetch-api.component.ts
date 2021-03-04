26839dee3b735ac1f0ea44a2f8e1dd1d97e5396f
Zahid Qureshi <zahidnqureshi@gmail.com>
2021-03-05 02:44:42 +0300

    Post request to api

--- src/app/fetch-api/fetch-api.component.ts
+++ src/app/fetch-api/fetch-api.component.ts
@@ -1,8 +1,5 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../assets/User';
-import { throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 import { ApiService } from '../http.service';
 
 @Component({
@@ -13,58 +10,24 @@ import { ApiService } from '../http.service';
 export class FetchAPIComponent implements OnInit {
 
   users: User[];
-  //arr1: any =[{id: 7, first_name:'Zahid', last_name:'Qureshi', email:'zahidnqureshi@gmail.com', avatar:'https://tiny.cc/public/images/robot_small.png'}];
+  user: any = {};
   
   constructor(private apiService:ApiService) { }
 
   ngOnInit(): void {
-    /*this.apiService.GetApi().subscribe(users => {
-        this.users = users;
-        console.log(users);
-    });*/
-    this.users= [
-        {
-            id: 7,
-            email: "michael.lawson@reqres.in",
-            first_name: "Michael",
-            last_name: "Lawson",
-            avatar: "https://reqres.in/img/faces/7-image.jpg"
-        },
-        {
-            id: 8,
-            email: "lindsay.ferguson@reqres.in",
-            first_name: "Lindsay",
-            last_name: "Ferguson",
-            avatar: "https://reqres.in/img/faces/8-image.jpg"
-        },
-        {
-            id: 9,
-            email: "tobias.funke@reqres.in",
-            first_name: "Tobias",
-            last_name: "Funke",
-            avatar: "https://reqres.in/img/faces/9-image.jpg"
-        },
-        {
-            id: 10,
-            email: "byron.fields@reqres.in",
-            first_name: "Byron",
-            last_name: "Fields",
-            avatar: "https://reqres.in/img/faces/10-image.jpg"
-        },
-        {
-            id: 11,
-            email: "george.edwards@reqres.in",
-            first_name: "George",
-            last_name: "Edwards",
-            avatar: "https://reqres.in/img/faces/11-image.jpg"
-        },
-        {
-            id: 12,
-            email: "rachel.howell@reqres.in",
-            first_name: "Rachel",
-            last_name: "Howell",
-            avatar: "https://reqres.in/img/faces/12-image.jpg"
-        }
-    ]
+    this.getUsers();
+    }
+
+  getUsers(){
+    this.apiService.GetApi().subscribe((data: any) => {
+        this.users = data.data;
+        console.log(this.users);
+    });
+  }
+
+  addUser(){
+    this.apiService.createUser(this.user).subscribe((res)=>{
+        console.log(this.user);
+    });
   }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,17 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { User } from '../assets/User';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  apiUrl:string = "https://reqres.in/api/users?page=1";
+  apiUrl:string = "https://reqres.in/api/users";
 
   constructor(private http: HttpClient) { }
 
-  GetApi(): Observable<User[]> {
-    return this.http.get<User[]>( this.apiUrl);
+  GetApi(){
+    return this.http.get( this.apiUrl).pipe(
+           map((data: User[]) => {
+             return data;
+           }), catchError( error => {
+             return throwError( 'ERROR!' );
+           })
+        )
   }
+
+  createUser(user: User[]) {
+    return this.http.post(this.apiUrl,user).
+        pipe(
+           map((data: any) => {
+             return data;
+           }), catchError( error => {
+             return throwError( 'ERROR!' );
+           })
+        )
+    }
 }
